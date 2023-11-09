84ab8f5010567dec8596a1e4aa98f42417f1dae6
Anisafauziah1 <fzhnsa@gmail.com>
2023-11-09 07:26:17 +0700

    first

---
+++ app/src/main/java/com/example/esjumbo/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo.Data
+
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.flavor1,
+        R.string.flavor2,
+        R.string.flavor3,
+        R.string.flavor4,
+        R.string.flavor5,
+    )
+}
