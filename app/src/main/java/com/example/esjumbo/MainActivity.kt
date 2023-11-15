86752fbf59a3c2e90a4ed3d0daece2ce4453b740
Anisafauziah1 <fzhnsa@gmail.com>
2023-11-16 01:08:52 +0700

    menambhakan function di order view

--- app/src/main/java/com/example/esjumbo/MainActivity.kt
+++ app/src/main/java/com/example/esjumbo/MainActivity.kt
@@ -16,11 +16,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           EsJumboTheme {
+            EsJumboTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorScheme.background
                 ) {
                     EsJumboApp()
                 }
@@ -29,10 +29,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    EsJumboTheme{
-        HalamanHome (onNextButtonClicked = {})
+    EsJumboTheme {
+        HalamanHome(onNextButtonClicked = {})
     }
 }
