c7c80006e10363faca0f58404eb5976f076bb9b4
Anisafauziah1 <fzhnsa@gmail.com>
2023-11-16 01:30:29 +0700

    finish

--- app/src/main/java/com/example/esjumbo/MainActivity.kt
+++ app/src/main/java/com/example/esjumbo/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.esjumbo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.esjumbo.EsJumboApp // Gantilah dengan package yang sesuai
+import com.example.esjumbo.EsJumboTheme // Gantilah dengan package yang sesuai
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +24,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = colorScheme.background
-                ) {
+                    color = MaterialTheme.colorScheme.background
+                ){
                     EsJumboApp()
                 }
             }
